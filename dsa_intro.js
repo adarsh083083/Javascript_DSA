092632ff648db2b9fceb97a4dff52a92664b5ac6
Adarsh raj <adarsh083083@gmail.com>
2024-04-23 03:11:03 +0530

    traversal question

---
+++ dsa_intro.js
@@ -0,0 +1,24 @@
+// *******************************************DataStructure and algorithm with javaScript***********************************************************
+
+// 1.&*********************************************Data Structure***********************************************************************************
+// *dataStructure
+//1.Primitive Data Structure==>Interger,float,character,pointer
+//**
+//2.Non-Primitive Data Structure==>Array ,List ,File
+
+// *List=>LINEAR LIST,NON-LINEAR-LISTS
+// **LINEAR LIST===>STACKS,QUEUES
+// **NON-LINEAR LISTS==>GRAPHS,TREENS
+
+
+//Operation of Data structure
+// 1.Traversal
+// 2.Insertion
+// 3.Deletion
+// 4.Searching
+// 5.Sorting
+// 6.Merging
+
+
+
+
